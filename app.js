54109816b027dafac6223dcecd26cc380b75d560
Ivan <mrkvvn@yandex.ru>
2022-10-14 23:47:12 +0300

    Random initial slide

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const slides = document.querySelectorAll('.slide');
+const randSlideIndex = Math.floor(Math.random() * slides.length);
 
-slides.forEach(slide => {
-	slide.addEventListener('click', () => {
-		clearAll();
-		slide.classList.add('active');
-	});
-});
+cardsSlider(randSlideIndex);
+
+function cardsSlider(initCard = 0) {
+
+	slides[initCard].classList.add("active");
+
+	slides.forEach(slide => {
+		slide.addEventListener('click', () => {
+			clearAll();
+			slide.classList.add('active');
+		});
+	});	
+}
 
 function clearAll() {
 	slides.forEach(slide => {
